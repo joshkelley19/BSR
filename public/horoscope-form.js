ec653f52a0eb501d647eb5f593d0a333cf812857
joshuakelley <joshkelley19@gmail.com>
2022-09-10 18:28:29 -0400

    convert to functional component

--- public/horoscope-form.js
+++ public/horoscope-form.js
@@ -1,28 +1,25 @@
-'use strict';
+// 'use strict';
+const { useState } = React;
 
 // import HoroscopeGrid from "./grid";
 
 const e = React.createElement;
 
-class HoroscopeForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sign: '',
-      horoscope: '',
-      month: '',
-      signs: ['Aries', 'Taurus', 'Gemini', 'Cancer',
-        'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius',
-        'Capricorn', 'Aquarius', 'Pisces'],
-      months: ['January', 'February', 'March', 'April',
-        'May', 'June', 'July', 'August', 'September',
-        'October', 'November', 'December'],
-      horoscopes: []
-    };
-  }
+function HoroscopeForm(props) {
+  const [sign, setSign] = useState('');
+  const [horoscope, setHoroscope] = useState('');
+  const [month, setMonth] = useState('');
+  const [signs, setSigns] = useState(['Aries', 'Taurus', 'Gemini', 'Cancer',
+    'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius',
+    'Capricorn', 'Aquarius', 'Pisces']);
+  const [months, setMonths] = useState(['January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August', 'September',
+    'October', 'November', 'December'])
+  const [horoscopes, setHoroscopes] = useState([]);
 
-  renderOptions(optionsStr, upper) {
-    const optionsList = this.state[optionsStr].map((val, index) => {
+
+  function renderOptions(options, upper) {
+    const optionsList = options.map((val, index) => {
       return (<option key={index} value={upper ? val.toUpperCase() : val}>{val}</option>);
     });
     optionsList.unshift((<option id="select-placeholder" key="select-placeholder" disabled selected value> -- select an option -- </option>
@@ -30,22 +27,22 @@ class HoroscopeForm extends React.Component {
     return optionsList;
   }
 
-  setValue(field, event) {
+  function setValue(setFunction, event) {
     console.log(field, event.target.value);
-    this.setState({ [field]: event.target.value });
+    setFunction(event.target.value);
   }
 
-  async submit() {
-    console.log('Submission', this.state);
-    const { sign: zodiac, horoscope: reading, month } = this.state;
+  async function submit() {
+    const body = {
+      zodiac: sign,
+      reading: horoscope,
+      month,
+      //TODO dynamically pull year
+      year: '2022'
+    }
+    console.log('Submission', body);
     const horoscopes = await fetch('https://becoming-spiritually-rich.herokuapp.com/horoscope', {
-      body: JSON.stringify({
-        zodiac,
-        reading,
-        month,
-        //TODO dynamically pull year
-        year: '2022'
-      }),
+      body: JSON.stringify(body),
       headers: {
         'Content-Type': 'application/json'
       },
@@ -53,34 +50,32 @@ class HoroscopeForm extends React.Component {
       mode: 'cors'
     })
     console.log('horoscopes', horoscopes);
-    this.setState({ horoscopes });
+    setHoroscopes(horoscopes);
   }
 
-  render() {
-    return <div>
-      <h1 className="text-center">Becoming Spiritually Rich</h1>
-      <div id="horoscope-fields">
-        <label htmlFor="sign-select" className="form-label">Sign</label>
-        <select id="sign-select" className="form-select form-select-med" aria-label="Sign select"
-          onChange={(e) => this.setValue('sign', e)}>
-          {this.renderOptions('signs', true)}
-        </select>
-        <label htmlFor="month-select" className="form-label">Horoscope</label>
-        <select id="month-select" className="form-select form-select-med" aria-label="Month select"
-          onChange={(e) => this.setValue('month', e)}>
-          {this.renderOptions('months', true)}
-        </select>
-        <label htmlFor="horoscope-input" className="form-label">Horoscope</label>
-        <textarea id="horoscope-input" className="form-control" maxLength="255"
-          onChange={(e) => this.setValue('horoscope', e)} />
-        <button type="button" className="btn btn-primary my-2"
-          onClick={() => this.submit()}>Submit</button>
-      </div>
-      <div id="horoscope-list">
-        {/* <HoroscopeGrid columns={Object.keys(this.state.horoscopes)} values={Object.values(this.state.horoscopes)} /> */}
-      </div>
+  return <div>
+    <h1 className="text-center">Becoming Spiritually Rich</h1>
+    <div id="horoscope-fields">
+      <label htmlFor="sign-select" className="form-label">Sign</label>
+      <select id="sign-select" className="form-select form-select-med" aria-label="Sign select"
+        onChange={(e) => setValue(setSign, e)}>
+        {renderOptions(signs, true)}
+      </select>
+      <label htmlFor="month-select" className="form-label">Horoscope</label>
+      <select id="month-select" className="form-select form-select-med" aria-label="Month select"
+        onChange={(e) => setValue(setMonth, e)}>
+        {renderOptions(months, true)}
+      </select>
+      <label htmlFor="horoscope-input" className="form-label">Horoscope</label>
+      <textarea id="horoscope-input" className="form-control" maxLength="255"
+        onChange={(e) => setValue(setHoroscope, e)} />
+      <button type="button" className="btn btn-primary my-2"
+        onClick={() => submit()}>Submit</button>
     </div>
-  }
+    <div id="horoscope-list">
+      {/* <HoroscopeGrid columns={Object.keys(this.state.horoscopes)} values={Object.values(this.state.horoscopes)} /> */}
+    </div>
+  </div>
 }
 
 const domContainer = document.querySelector('#horoscope-form');
