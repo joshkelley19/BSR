29edeab0c9282dabfa331ea1ccc004e5ae34ef44
joshuakelley <joshkelley19@gmail.com>
2022-11-22 20:41:42 -0500

    grid by type, controlled form fields

--- public/grid.js
+++ public/grid.js
@@ -1,25 +1,55 @@
 
-export function HoroscopeGrid(props) {
+function createGridConfig(columns, data, blacklist = [], customMappers = {}, selectionFunc = null) {
+  // TODO add date formatters
+  const mappedColumns = columns.filter(c => !blacklist.includes(c)).map(c => Object.assign({}, {
+    id: c,
+    name: c,
+    sort: true
+  },
+    !!customMappers[c] && { data: customMappers[c] }));
+
+  mappedColumns.unshift({
+    id: 'select',
+    name: 'select',
+    formatter: (cell, row, column) => gridjs.h('button', {
+      className: 'btn btn-primary my-2',
+      onClick: () => {
+        row = row.toArray();
+        selectionFunc(Object.assign({}, {type: row[1], sign: row[2], endDate: row[4]}))
+      }
+    }, 'Select')
+  })
+  return {
+    columns: mappedColumns,
+    data,
+    search: true
+  }
+}
+
+function getCustomMappers() {
+  return {
+    postings: (category) => {
+      return category.postings[0].description;
+    }
+  }
+}
+
+export function HoroscopeGrid({ columns, values, blacklist, setCategory }) {
   const wrapperRef = React.useRef(null);
   const [grid, setGrid] = useState(new gridjs.Grid({
-    columns: ['name', 'date']/* props.columns */,
-    data: [['Josh', 'today']]/* props.values */,
-    sort: true
+    columns: [],
+    data: [[]]
   }));
 
   React.useEffect(() => {
-    console.log('new grid props', props, grid);
     grid.render(wrapperRef.current);
   }, []);
 
-  if (props.values.length) {
-    console.log('forcing render', props, grid);
-    grid.updateConfig({
-      search: true,
-      columns: props.columns,
-      data: props.values
-    }).forceRender();
-  }
+  React.useEffect(() => {
+    console.log('new grid props', columns, values, grid);
+    const config = createGridConfig(columns, values, blacklist, getCustomMappers(), setCategory);
+    grid.updateConfig(config).forceRender();
+  }, [values, columns]);
 
   return <div id="grid-wrapper-el" ref={wrapperRef} />;
 };
--- public/horoscope-form.js
+++ public/horoscope-form.js
@@ -1,23 +1,49 @@
 // 'use strict';
-const { useState } = React;
+const { useState, useEffect } = React;
 
 // import HoroscopeGrid from "./grid";
 
 const e = React.createElement;
 
+async function getAllCategoriesByType(type, columnSetter, valueSetter) {
+  const categories = await (await fetch(`http://localhost:8080/api/horoscope/categories/${type}`)).json();
+  if (categories.length) {
+    columnSetter(Object.keys(categories[0]));
+    valueSetter(categories);
+  }
+}
+
+function setCategoryFormFields(setType, setSign, setStartDate, category) {
+  setType(category.type);
+  setSign(category.sign);
+  // TODO add 1 second
+  setStartDate(category.endDate + 1);
+}
+
 function HoroscopeForm(props) {
-  const [sign, setSign] = useState('');
+  const [sign, setSign] = useState(null);
+  const [header, setHeader] = useState('');
   const [horoscope, setHoroscope] = useState('');
-  const [month, setMonth] = useState('');
-  const [signs, setSigns] = useState(['Aries', 'Taurus', 'Gemini', 'Cancer',
+  const [interval, setInterval] = useState('');
+  const [type, setType] = useState('');
+  // TODO pull categories and signs from backend
+  const [signs] = useState(['Aries', 'Taurus', 'Gemini', 'Cancer',
     'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius',
     'Capricorn', 'Aquarius', 'Pisces']);
-  const [months, setMonths] = useState(['January', 'February', 'March', 'April',
-    'May', 'June', 'July', 'August', 'September',
-    'October', 'November', 'December'])
-  const [horoscopes, setHoroscopes] = useState([]);
+  const [intervals] = useState(['None', 'Monthly', 'Weekly', 'Daily']);
+  const [types] = useState(["Horoscope", "Sun", "Current Moon", "Rising", "New Moon",
+    "Full Moon", "Mercury", "Venus", "Earth", "Mars",
+    "Saturn", "Jupiter", "Uranus", "Neptune", "Pluto"]);
+  const [tableValues, setTableValues] = useState([]);
   const [tableFields, setTableFields] = useState([]);
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
 
+  useEffect(() => {
+    if (type) {
+      getAllCategoriesByType(type, setTableFields, setTableValues);
+    }
+  }, [type])
 
   function renderOptions(options, upper) {
     const optionsList = options.map((val, index) => {
@@ -34,16 +60,21 @@ function HoroscopeForm(props) {
 
   async function submitHoroscope() {
     const body = {
-      zodiac: sign,
-      reading: horoscope,
-      month,
-      //TODO dynamically pull year
-      year: '2022'
+      type,
+      sign,
+      header,
+      description: horoscope,
+      increment: interval === 'NONE' ? null : interval,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      active: true,
+      category: null
     }
     console.log('Submission', body);
     let horoscopeVal = [];
     try {
-      const res = await fetch('https://becoming-spiritually-rich.herokuapp.com/horoscope', {
+      // const res = await fetch('https://becoming-spiritually-rich.herokuapp.com/horoscope', {
+      const res = await fetch('http://localhost:8080/api/horoscope', {
         body: JSON.stringify(body),
         headers: {
           'Content-Type': 'application/json'
@@ -52,46 +83,65 @@ function HoroscopeForm(props) {
         mode: 'cors'
       });
       horoscopeVal = await res.json();
+      console.log('horoscopes', horoscopeVal);
+      setTableFields(horoscopeVal.length ? Object.keys(horoscopeVal[0]) : []);
+      setTableValues(horoscopeVal);
+      // setValues();
     } catch (e) {
       console.error('fetch error', e);
-    }
-    console.log('horoscopes', horoscopes);
-    setTableFields(horoscopeVal.length ? Object.keys(horoscopeVal[0]) : []);
-    setHoroscopes(horoscopeVal);
-    setValues();
-  }
-
-  function setValues(horoscopeObj) {
-    if(horoscopeObj) {
-
-    } else {
-      setHoroscope('');
-      setMonth('');
-      setSign('');
     }
   }
 
   return <div>
     <h1 className="text-center">Becoming Spiritually Rich</h1>
     <div id="horoscope-fields">
-      <label htmlFor="sign-select" className="form-label">Sign</label>
-      <select id="sign-select" className="form-select form-select-med" aria-label="Sign select"
-        onChange={(e) => setValue(setSign, e)}>
-        {renderOptions(signs, true)}
-      </select>
-      <label htmlFor="month-select" className="form-label">Month</label>
-      <select id="month-select" className="form-select form-select-med" aria-label="Month select"
-        onChange={(e) => setValue(setMonth, e)}>
-        {renderOptions(months, true)}
-      </select>
-      <label htmlFor="horoscope-input" className="form-label">Horoscope</label>
-      <textarea id="horoscope-input" className="form-control" maxLength="255"
-        onChange={(e) => setValue(setHoroscope, e)} />
+      <div className="form-group">
+        <label htmlFor="type-select" className="form-label">Type</label>
+        <select id="type-select" className="form-select form-select-med" aria-label="Type select"
+          onChange={(e) => setValue(setType, e)} value={type}>
+          {renderOptions(types)}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="sign-select" className="form-label">Sign</label>
+        <select id="sign-select" className="form-select form-select-med" aria-label="Sign select"
+          onChange={(e) => setValue(setSign, e)} value={sign} >
+          {renderOptions(signs, true)}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="interval-select" className="form-label">Interval</label>
+        <select id="interval-select" className="form-select form-select-med" aria-label="Interval select"
+          onChange={(e) => setValue(setInterval, e)} value={interval}>
+          {renderOptions(intervals, true)}
+        </select>
+      </div>
+      <div className="d-flex flex-row">
+        <div className="form-group">
+          <label htmlFor="start-date" className="form-label">Start Date</label>
+          <input type="datetime-local" className="form-control" onChange={(e) => setValue(setStartDate, e)} value={startDate} />
+        </div>
+        <div className="form-group px-3">
+          {!interval || interval == 'NONE' ? <div><label htmlFor="end-date" className="form-label">End Date</label>
+            <input type="datetime-local" className="form-control" onChange={(e) => setValue(setEndDate, e)} value={endDate} /></div> : ''}
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="header-input" className="form-label">Header</label>
+        <input id="header-input" className="form-control" maxLength="120"
+          onChange={(e) => setValue(setHeader, e)} value={header} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="horoscope-input" className="form-label">Horoscope</label>
+        <textarea id="horoscope-input" className="form-control" maxLength="255"
+          onChange={(e) => setValue(setHoroscope, e)} value={horoscope} />
+      </div>
       <button type="button" className="btn btn-primary my-2"
         onClick={() => submitHoroscope()}>Submit</button>
     </div>
     <div id="horoscope-list">
-      <HoroscopeGrid columns={tableFields} values={horoscopes} />
+      <HoroscopeGrid columns={tableFields} values={tableValues} blacklist={['id', 'name', 'active']} setCategory={setCategoryFormFields.bind(null, setType, setSign, setStartDate)} />
     </div>
   </div>
 }
