ee44639d797bf00074b9f0b277af19ae9f6fc6ae
joshuakelley <joshkelley19@gmail.com>
2023-01-01 21:35:20 -0500

    add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Auth from './auth';
 import { HoroscopeForm } from './horoscope-form';
 import Marketing from './marketing';
 import Navigation from './navigation';
-import { getAppCount, initApp, getBaseUrl } from './services/firebase-service';
+import { getAppCount, initApp, getBaseUrl, onAuthStateChanged, checkAdmin, signOut } from './services/firebase-service';
 
 
 const tabConfig = [{
@@ -15,13 +16,13 @@ const tabConfig = [{
   id: 'marketing'
 }]
 
-const render = (activeTab: string, firebase: any, baseUrl: string) => {
+const render = (activeTab: string, firebase: any, baseUrl: string, setErrorMessage: Function) => {
   switch (activeTab) {
     case 'marketing':
-      return <Marketing firebase={firebase} />
+      return <Marketing firebase={firebase} setErrorMessage={setErrorMessage} />
     case 'horoscopes':
     default:
-      return <HoroscopeForm firebase={firebase} baseUrl={baseUrl} />
+      return <HoroscopeForm firebase={firebase} baseUrl={baseUrl} setErrorMessage={setErrorMessage} />
   }
 }
 
@@ -29,14 +30,24 @@ function App() {
   const [firebase, setFirebase] = useState({});
   const [baseUrl, setBaseUrl] = useState('');
   const [activeTab, setActiveTab] = useState(tabConfig[0].id);
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     if (!getAppCount()) {
       const initialize = async () => {
         const fb = await initApp();
         setFirebase(fb);
-        const url = await getBaseUrl(fb.db);
-        setBaseUrl(url);
+        onAuthStateChanged(async (user: any) => {
+          setUser(user);
+          const url = await getBaseUrl(fb.db);
+          setBaseUrl(url);
+          checkAdmin(user, setIsAdmin, setErrorMessage);
+        },
+          (err: any) => {
+            console.error('Error getting auth state: ', err);
+          });
       }
       initialize();
     }
@@ -44,9 +55,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-center">Becoming Spiritually Rich</h1>
-      <Navigation activeTab={activeTab} setActiveTab={setActiveTab} tabConfig={tabConfig} />
-      {render(activeTab, firebase, baseUrl)}
+      <h1 className="text-center">
+        Becoming Spiritually Rich - Admin
+        <button className="btn btn-danger m-2" onClick={() => { signOut() }}>Sign Out</button>
+      </h1>
+      {errorMessage ? <div className="alert alert-danger alert-dismissible fade show" role="alert">
+        {errorMessage}
+        <button type="button" className="btn-close" onClick={() => setErrorMessage('')} data-bs-dismiss="alert" aria-label="Close"></button>
+      </div> : <div></div>}
+      {isAdmin ?
+        <div>
+          <Navigation activeTab={activeTab} setActiveTab={setActiveTab} tabConfig={tabConfig} />
+          {render(activeTab, firebase, baseUrl, setErrorMessage)}
+        </div>
+        :
+        <Auth setErrorMessage={setErrorMessage} />}
     </div>
   );
 }
